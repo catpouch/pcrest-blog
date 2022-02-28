3bdabea71502edbe3fee52b6c9d576aaa5c2a32c
CatSack <bossness544@gmail.com>
2022-02-27 18:19:34 -0800

    very small amount of post page styling

--- pages/posts/[post].js
+++ pages/posts/[post].js
@@ -5,6 +5,7 @@ import DefaultErrorPage from 'next/error'
 import { useRouter } from 'next/router'
 import styles from './post.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Post(props) {
     const router = useRouter()
@@ -25,6 +26,9 @@ export default function Post(props) {
     return (
         <div>
             <div className={styles.contentWrapper}>
+                <Link href='/'>
+                    <a className={styles.home}><span id={styles.one}>&#8592; Return to home</span><span id={styles.two}>&#8592;</span></a>
+                </Link>
                 <div className={styles.textWrapper}>
                     <div className={styles.title}><h1>{props.frontmatter.title}</h1>Posted on {props.frontmatter.date} by {props.frontmatter.author}</div>
                     <div className={styles.thumbnail}>
