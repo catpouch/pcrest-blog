e6130f204542defc531c9a3bd67f84f1f68368a5
CatSack <bossness544@gmail.com>
2022-02-28 20:11:38 -0800

    deletion authentication, removed from post page better back to home page button

--- pages/api/delete_post/[delete_post].js
+++ pages/api/delete_post/[delete_post].js
@@ -1,10 +1,20 @@
 import fs from 'fs'
+import { getSession } from 'next-auth/react'
+import permissions from '../../../user_permissions.json'
 
 export default async function handler(req, res) {
+    const session = await getSession({ req })
+    if(!session || !permissions.admins.includes(session.user.email)) {
+        return res.status(403).end()
+    }
     if(req.method !== 'DELETE') {
         return res.status(405).send('Only DELETE requests allowed!')
     }
     const { delete_post } = await req.query
-    fs.unlinkSync('./posts/' + delete_post + '.md')
-    res.status(204).end()
+    try {
+        fs.unlinkSync('./posts/' + delete_post + '.md')
+    } catch(e) {
+        return res.status(410).end()
+    }
+    return res.status(204).end()
 }
--- pages/posts/[post].js
+++ pages/posts/[post].js
@@ -2,21 +2,11 @@ import fs from 'fs'
 import matter from 'gray-matter'
 import ReactMarkdown from 'react-markdown'
 import DefaultErrorPage from 'next/error'
-import { useRouter } from 'next/router'
 import styles from './post.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Post(props) {
-    const router = useRouter()
-
-    async function delete_post(e) {
-        await fetch(`/api/delete_post/${props.post}`, {
-            method: 'DELETE'
-        })
-        router.push('/')
-    }
-
     if(!props) {
         return (
             <DefaultErrorPage statusCode={404}/>
@@ -27,7 +17,10 @@ export default function Post(props) {
         <div>
             <div className={styles.contentWrapper}>
                 <Link href='/'>
-                    <a className={styles.home}><span id={styles.one}>&#8592; Return to home</span><span id={styles.two}>&#8592;</span></a>
+                    <div className={styles.home}>
+                        <span id={styles.one}>&#8592; Return to home</span>
+                        <span id={styles.two}>&#8592;</span>
+                    </div>
                 </Link>
                 <div className={styles.textWrapper}>
                     <div className={styles.title}><h1>{props.frontmatter.title}</h1>Posted on {props.frontmatter.date} by {props.frontmatter.author}</div>
@@ -37,7 +30,6 @@ export default function Post(props) {
                     <ReactMarkdown>{props.content}</ReactMarkdown>
                 </div>
             </div>
-            <button onClick={delete_post}>delete post</button>
         </div>
     )
 }
